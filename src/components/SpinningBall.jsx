4ce632f5312714585a823f506921cf8fff7829d7
Kevin Smith Jimenez Garro <kevinedk33@gmail.com>
2024-06-04 16:12:51 -0500

    Just the moffin

--- src/components/SpinningBall.jsx
+++ src/components/SpinningBall.jsx
@@ -14,30 +14,34 @@ const MuffinModel = () => {
     }
   });
 
-  return <primitive ref={meshRef} object={scene} rotation={[1.6, 0, 0]} scale={[2.2, 2.2, 2.2]} />;
+  return <primitive ref={meshRef} object={scene} rotation={[1.6, 0, 0]} scale={[0.6, 0.6, 0.6]} />;
 };
 
 const Scene = () => {
   return (
-    <div className='flex items-center justify-center h-screen w-full'>
-      <div className='relative w-full'>
-        <div className='absolute inset-0 flex items-center justify-center'>
-          <div className='rounded-full border-2 border-white w-[200px] h-[200px] bg-[#61B5AB] '></div>
-          <div className='bg-black z-20 w-[200px] h-[200px] rounded-full opacity-50 absolute'></div>
-        </div>
+    <Canvas className='w-full'>
+      <ambientLight intensity={2.3} />
+      <MuffinModel />
+    </Canvas>
+    // <div className='flex items-center justify-center h-screen w-full'>
+    //   <div className='relative w-full'>
+    //     <div className='absolute inset-0 flex items-center justify-center'>
+    //       <div className='rounded-full border-2 border-white w-[200px] h-[200px] bg-[#61B5AB] '></div>
+    //       <div className='bg-black z-20 w-[200px] h-[200px] rounded-full opacity-50 absolute'></div>
+    //     </div>
 
-        <div className='relative z-20'>
-          <div className='relative z-30'>
-            <Canvas className='w-full'>
-              <ambientLight intensity={2.3} />
-              <MuffinModel />
-            </Canvas>
-          </div>
+    //     <div className='relative z-20'>
+    //       <div className='relative z-30'>
+    //         <Canvas className='w-full'>
+    //           <ambientLight intensity={2.3} />
+    //           <MuffinModel />
+    //         </Canvas>
+    //       </div>
 
-          <img className='absolute right-1/2 translate-x-1/2 bottom-1/2 translate-y-1/2 z-10 w-[170px]' src='/bg.svg' alt='bg' />
-        </div>
-      </div>
-    </div>
+    //       <img className='absolute right-1/2 translate-x-1/2 bottom-1/2 translate-y-1/2 z-10 w-[170px]' src='/bg.svg' alt='bg' />
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
