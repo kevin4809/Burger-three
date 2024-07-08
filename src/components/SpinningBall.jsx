d49fc0da88acb7d7f83e4c388a40961ab573b407
Kevin Smith Jimenez Garro <kevinedk33@gmail.com>
2024-07-08 18:20:22 -0500

    Last touch

--- src/components/SpinningBall.jsx
+++ src/components/SpinningBall.jsx
@@ -1,12 +1,21 @@
 'use client';
 
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
+import * as THREE from 'three';
 
 const MuffinModel = () => {
   const { scene } = useGLTF('/muffin.glb');
   const meshRef = useRef();
+  const { camera, gl } = useThree();
+  const [mouse] = useState(() => new Vector2());
+
+  const handleTouch = () => {
+    alert('Funciona mamahuevossss');
+    console.log('Mama tengo hambre');
+    window.location.href = 'https://www.youtube.com/watch?v=RHd7CrVHUx8';
+  };
 
   useFrame(() => {
     if (meshRef.current) {
@@ -14,11 +23,30 @@ const MuffinModel = () => {
     }
   });
 
-  const handleClick = () => {
-    console.log('Funciona Mamahuevoooooooooooooooooo');
+  const handlePointerDown = (event) => {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    const raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObject(meshRef.current, true);
+
+    if (intersects.length > 0) {
+      handleTouch();
+    }
   };
 
-  return <primitive ref={meshRef} object={scene} rotation={[1.6, 0, 0]} scale={[0.6, 0.6, 0.6]} onClick={handleClick} />;
+  React.useEffect(() => {
+    const canvas = gl.domElement;
+    canvas.addEventListener('pointerdown', handlePointerDown);
+
+    return () => {
+      canvas.removeEventListener('pointerdown', handlePointerDown);
+    };
+  }, [gl.domElement]);
+
+  return <primitive ref={meshRef} object={scene} rotation={[1.6, 0, 0]} scale={[0.6, 0.6, 0.6]} />;
 };
 
 const Scene = () => {
