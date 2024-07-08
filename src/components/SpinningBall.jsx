ca1ac007d9dadd1c4aa6629f28b8960fbb27ac8b
Kevin Smith Jimenez Garro <kevinedk33@gmail.com>
2024-07-08 18:22:56 -0500

    Fixz

--- src/components/SpinningBall.jsx
+++ src/components/SpinningBall.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
 import * as THREE from 'three';
@@ -8,8 +8,6 @@ import * as THREE from 'three';
 const MuffinModel = () => {
   const { scene } = useGLTF('/muffin.glb');
   const meshRef = useRef();
-  const { camera, gl } = useThree();
-  const [mouse] = useState(() => new Vector2());
 
   const handleTouch = () => {
     alert('Funciona mamahuevossss');
@@ -23,30 +21,7 @@ const MuffinModel = () => {
     }
   });
 
-  const handlePointerDown = (event) => {
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-    const raycaster = new THREE.Raycaster();
-    raycaster.setFromCamera(mouse, camera);
-
-    const intersects = raycaster.intersectObject(meshRef.current, true);
-
-    if (intersects.length > 0) {
-      handleTouch();
-    }
-  };
-
-  React.useEffect(() => {
-    const canvas = gl.domElement;
-    canvas.addEventListener('pointerdown', handlePointerDown);
-
-    return () => {
-      canvas.removeEventListener('pointerdown', handlePointerDown);
-    };
-  }, [gl.domElement]);
-
-  return <primitive ref={meshRef} object={scene} rotation={[1.6, 0, 0]} scale={[0.6, 0.6, 0.6]} />;
+  return <primitive ref={meshRef} object={scene} rotation={[1.6, 0, 0]} scale={[0.6, 0.6, 0.6]} onClick={handleTouch} />;
 };
 
 const Scene = () => {
